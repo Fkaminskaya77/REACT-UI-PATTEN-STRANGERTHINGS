66b1397cdf0e61fb5a8c0158b3a72a8e00210c00
flo777 <fkaminskaya21@gmail.com>
2023-02-19 20:20:30 -0700

    Setup of Tab Component pages and setup of tabs

--- strangerthings-project/src/App.js
+++ strangerthings-project/src/App.js
@@ -2,6 +2,7 @@ import './Components/Tabs'
 import React, { useState, useEffect } from 'react';
 import './index'
 import './App.css';
+import Tabs from'./Components/Tabs'
 
 
   function App() {
@@ -21,17 +22,19 @@ import './App.css';
   
  //Getting data to populate on Page using .map 
  // Author would'nt pop up from array added my own little extra edit
+ //rendering Tabs from Tab Component to main page(App.js)
  return(
   <main className="App">
     <h1 className='heading'>Stranger Things Quotes</h1>
-
   <div className='quotes'></div>
   {quotes.map((quotes, index) => (
     <li key={index}> {quotes.quote} Author: {quotes.author}</li>
   )
-  
-  )}
-   
+ )}
+    <div>
+  <Tabs/>
+  </div>
+
   </main>
 
    );
--- strangerthings-project/src/Components/Tabs.js
+++ strangerthings-project/src/Components/Tabs.js
@@ -2,7 +2,43 @@ import './Tabs.css'
 
 //Creating Tabs for the UI
 function Tabs () {
-    <div>
-        <h1>Tabs</h1>
-    </div>
+    return(
+
+<div className="container">
+            
+        <div className='nav-tabs'>
+        <div className='tabs active-tabs'>Tab 1</div>
+        <div className='tabs'>Tab 2</div>
+        <div className='tabs'>Tab 3</div>
+        <div className='tabs'>Tab 4</div>
+        </div>
+
+    
+        <div className="content-tabs">
+            <div className="content">
+            <h2>Quotes:</h2>
+            <p>data array </p>
+            </div>
+            </div>
+
+        <div className="content">
+            <h2>Quotes:</h2>
+            <p>data array </p>
+            </div>
+            
+        
+        <div className="content">
+            <h2>Quotes:</h2>
+            <p>data array </p>
+            </div>
+            
+        <div className="content">
+            <h2>Quotes:</h2>
+            <p>data array </p>
+            </div>
+        
+        
+        </div>
+    );
 }
+export default Tabs;
