fd84d95276daa4225f02eb118a6de9b6ce7b71f9
flo777 <fkaminskaya21@gmail.com>
2023-02-22 23:50:19 -0700

    last edit now completed project

--- strangerthings-project/src/Components/Tabs.js
+++ strangerthings-project/src/Components/Tabs.js
@@ -19,33 +19,31 @@ const toggleTab = (index) => {
 
          <div className='nav-tabs'>
 
-             <button className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          <button className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
                  onClick={() => toggleTab(1)}>Quote 1</button>
 
-             <button className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          <button className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
                  onClick={() => toggleTab(2)}>Quote 2</button>
 
-             <button className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          <button className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
                  onClick={() => toggleTab(3)}>Quote 3</button>
 
-             <button className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+          <button className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
                  onClick={() => toggleTab(4)}>Quote 4</button>
          
-            <button className={toggleState === 5 ? "tabs active-tabs" : "tabs"}
+          <button className={toggleState === 5 ? "tabs active-tabs" : "tabs"}
                 onClick={() => toggleTab(5)}>Quote 5</button>
     
     
      </div>
      
      <div className="content-tabs">
-             {quotes.map((quote, index) => (
+                {quotes.map((quote, index) => (
                  <div key={index} className="content-tabs">
-                     <div
-                         className={toggleState === index + 1 ? "content  active-content" : "content"}
-                         >
-                        
-                         <p>{quote.quote}</p>
-                         <p>Character : {quote.author}</p>
+                 
+                 <div className={toggleState === index + 1 ? "content  active-content" : "content"}>
+                            <p>{quote.quote}</p>
+                            <p>Character : {quote.author}</p>
                      </div>
                  </div>
              ))}
